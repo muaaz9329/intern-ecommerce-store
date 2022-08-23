0d32d900ede3321a23ca1e68efb84ac55dbb98e0
muaaz9329 <63157500+muaaz9329@users.noreply.github.com>
2022-08-23 19:12:21 +0500

    touchEvent

--- public/script/homePage.js
+++ public/script/homePage.js
@@ -5,14 +5,17 @@ function checkLogin(){
       document.getElementById('login').innerText= localStorage.getItem('username')
     }
   }
-  document.getElementById('firstEventParent').addEventListener('mouseenter',()=>{
-    document.getElementById('firstEvent').classList.replace('invisible','visible')
-    document.getElementById('firstEvent').classList.add("animate__fadeInUp","animate__slow")
+  ['mouseenter','touchstart'].forEach((evt)=>{
+    document.getElementById('firstEventParent').addEventListener(evt,()=>{
+      document.getElementById('firstEvent').classList.replace('invisible','visible')
+      document.getElementById('firstEvent').classList.add("animate__fadeInUp","animate__slow")
+    })
+    document.getElementById('aboutSection').addEventListener(evt,()=>{
+      document.getElementById('aboutImg').classList.replace('invisible','visible')
+      
+      document.getElementById('aboutHead').classList.replace('invisible','visible')
+      document.getElementById('aboutImg').classList.add('animate__fadeInLeft','animate__slower')
+      document.getElementById('aboutHead').classList.add('animate__fadeInRight','animate__slower')
+    })
   })
-  document.getElementById('aboutSection').addEventListener('mouseenter',()=>{
-    document.getElementById('aboutImg').classList.replace('invisible','visible')
-    
-    document.getElementById('aboutHead').classList.replace('invisible','visible')
-    document.getElementById('aboutImg').classList.add('animate__fadeInLeft','animate__slower')
-    document.getElementById('aboutHead').classList.add('animate__fadeInRight','animate__slower')
-  })
+  
